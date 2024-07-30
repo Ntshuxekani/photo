38a9e475693b1b8aedaf85f014f085e6753c76a9
Ntshuxekani <ntshuxekanizitha@gmail.com>
2024-07-30 15:52:20 +0200

    springboot

---
+++ snapbooking/src/test/java/com/photoshoot/snapbooking/SnapbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photoshoot.snapbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
