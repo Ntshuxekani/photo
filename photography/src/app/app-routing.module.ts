180ecfc8e5c9f800f39a37a58ab68592b9b46b47
Ntshuxekani <ntshuxekanizitha@gmail.com>
2024-07-30 16:24:41 +0200

    navbar

--- photography/src/app/app-routing.module.ts
+++ photography/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { ProfileComponent } from './Components/profile/profile.component';
+import { PortfolioComponent } from './Components/portfolio/portfolio.component';
+import { BookingComponent } from './Components/booking/booking.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'booking', component: BookingComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'navbar', component: NavbarComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
